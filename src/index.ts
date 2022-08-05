7cbf0b27fd22493010ab1148c097ddd19070983a
Samuellsam <sagitariussamuel@gmail.com>
2022-08-05 23:35:20 +0700

    Setup cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import "dotenv/config";
 import express, { Application } from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const port = process.env.PORT; // Get port
 
 const app: Application = express(); // Create express application
+
+app.use(cors({ // Add cors
+  origin: '*'
+}));
+
 const server: http.Server = http.createServer(app);
 const io: Server = new Server(server);
 
